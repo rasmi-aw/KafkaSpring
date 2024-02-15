16d3af62189693185cb171a0d0e01fe3ab8ee516
rasmi-aw <abdelwadoudrasmi@gmail.com>
2024-02-15 12:03:41 +0100

    Init

--- src/main/java/com/beastwall/kafkaspring/KafkaSpringApplication.java
+++ src/main/java/com/beastwall/kafkaspring/KafkaSpringApplication.java
@@ -1,13 +1,44 @@
 package com.beastwall.kafkaspring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
 
 @SpringBootApplication
-public class KafkaSpringApplication {
+@RestController("/")
+public class KafkaSpringApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KafkaSpringApplication.class, args);
-	}
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
 
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaSpringApplication.class, args);
+        //
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+    }
+
+    @GetMapping("")
+    public void sendMessage(@RequestParam(required = false) String message) {
+        System.out.println(message);
+        kafkaTemplate.send("spring-kafka", message);
+
+    }
+
+    @KafkaListener(topics = "spring-kafka", groupId = "group-spring-kafka")
+    public void readMessage(String message) {
+        System.out.println("received message consumer " + message);
+    }
 }
--- src/main/java/com/beastwall/kafkaspring/config/KafkaConfig.java
+++ src/main/java/com/beastwall/kafkaspring/config/KafkaConfig.java
@@ -1,4 +1,30 @@
 package com.beastwall.kafkaspring.config;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
 public class KafkaConfig {
+
+    @Value(value = "${spring.kafka.bootstrap-servers}")
+    private String boostrapServerAddress;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, boostrapServerAddress);
+        return new KafkaAdmin(config);
+    }
+
+    @Bean
+    public NewTopic topic() {
+        return new NewTopic("spring-kafka", 1, (short) 1);
+    }
 }
